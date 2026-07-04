77f826f76e3f1a6e8ee04d5a0a3ba5cd854d05e7
deddyhp <deddyhendraputra@gmail.com>
2026-07-05 06:47:21 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="DHP-Lifes",
+    page_icon="❤️",
+    layout="wide"
+)
+
+st.title("❤️ DHP-Lifes")
+
+st.subheader("Welcome Deddy & Family")
+
+st.write("Selamat datang di DHP-Lifes.")
+
+st.success("Versi 1.0 berhasil berjalan 🚀")
